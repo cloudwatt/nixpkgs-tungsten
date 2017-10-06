9e4c5bb88ec1c95b93583c54932a6482de31cb3f
lewo <lewo@abesis.fr>
2017-10-06 12:38:22 +0200

    ci: listen on port 80

--- ci/hydra-configuration.nix
+++ ci/hydra-configuration.nix
@@ -5,20 +5,27 @@
 {
   imports = [ <nixpkgs/nixos/modules/virtualisation/nova-config.nix> ];
 
-  networking.firewall.allowedTCPPorts = [ 22 3000 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 22 80 3000 ];
+    # because hydra user is not allowed to bind port 80
+    extraCommands = ''
+      iptables -t nat -A PREROUTING -p TCP --dport 80 -j REDIRECT --to-port 3000
+      '';
+  };
 
   # Switch to kernel 4.4 because nested virtualisation is broken
   # with kernel 4.9. See https://github.com/NixOS/nixpkgs/issues/27930
   boot.kernelPackages = pkgs.linuxPackages_4_4;
 
-  services.hydra.enable = true;
-  services.hydra.hydraURL = "http://example.com";
-  services.hydra.notificationSender = "hydra@example.com";
-  services.hydra.extraConfig = ''
-    max_output_size = 4294967296
-    binary_cache_secret_key_file = /etc/nix/hydra-1/secret
-  '';
-  # services.hydra.debugServer = true;
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://example.com";
+    notificationSender = "hydra@example.com";
+    extraConfig = ''
+      max_output_size = 4294967296
+      binary_cache_secret_key_file = /etc/nix/hydra-1/secret
+    '';
+  };
 
   nix = {
     distributedBuilds = true;
