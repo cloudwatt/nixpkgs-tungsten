802f1fb72f7b53f7d9ef83b67339a55097032cfc
lewo <lewo@abesis.fr>
2017-10-12 18:23:06 +0200

    Remove useless deps

--- deps.nix
+++ deps.nix
@@ -98,36 +98,6 @@ rec {
     propagatedBuildInputs = [ pkgs.pythonPackages.thrift ];
   };
 
-  ubuntuKernelHeaders_3_13_0_83_generic = pkgs.stdenv.mkDerivation rec {
-    name = "ubuntuKernelHeaders-3.13.0-83-generic";
-    phases = [ "unpackPhase" "installPhase" ];
-    buildInputs = [ pkgs.dpkg ];
-    unpackCmd = "dpkg-deb --extract $curSrc tmp/";
-    # Packages url can be foung by browsing https://packages.ubuntu.com/trusty-updates/linux-headers-3.13.0-83-generic
-    srcs = [
-      (pkgs.fetchurl {
-        url = http://fr.archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-3.13.0-83-generic_3.13.0-83.127_amd64.deb;
-        sha256 = "f8b5431798c315b7c08be0fb5614c844c38a07c0b6656debc9cc8833400bdd98";
-      })
-      (pkgs.fetchurl {
-        url = http://fr.archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-3.13.0-83_3.13.0-83.127_all.deb;
-        sha256 = "7281be1ab2dc3b5627ef8577402fd3e17e0445880d22463e494027f8e904e8fa";
-      })
-    ];
-    installPhase = ''
-      mkdir -p $out
-      ${pkgs.rsync}/bin/rsync -rl * $out/
-
-      # We patch these scripts since they have been compiled for ubuntu
-      for i in recordmcount basic/fixdep mod/modpost; do
-        ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.stdenv.glibc}/lib/ld-linux-x86-64.so.2 $out/usr/src/linux-headers-3.13.0-83-generic/scripts/$i
-        ${pkgs.patchelf}/bin/patchelf --set-rpath ${pkgs.stdenv.glibc}/lib $out//usr/src/linux-headers-3.13.0-83-generic/scripts/$i
-      done
-
-      ln -sf $out/usr/src/linux-headers-3.13.0-83-generic $out/lib/modules/3.13.0-83-generic/build
-    '';
-  };
-
   promptToolkit = pkgs.pythonPackages.buildPythonPackage rec {
     pname = "prompt_toolkit";
     version = "1.0.15";
