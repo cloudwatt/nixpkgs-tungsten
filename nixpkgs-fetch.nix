4172b0099a688fc923c787d70676831bb5aa6a6f
Antoine Eiche <lewo@abesis.fr>
2018-01-23 11:52:20 +0100

    Update nixpkgs

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -3,9 +3,9 @@
 let bootstrap_pkgs = import nixpkgs {};
 in {
   pkgs = bootstrap_pkgs.fetchFromGitHub {
-    owner = "nlewo";
+    owner = "NixOS";
     repo = "nixpkgs";
     # Belong to the branch release-17.09-cloudwatt
-    rev = "cb5ac615101c5ef346a88648f918685f39f335d8";
-    sha256 = "0lrn1vvbli82zkcq32dkkj56b1l008bmpw2zyhmsw85ajqw8imab";};
+    rev = "92d088e891edee8dba99b1cab6889445b6ad3bee";
+    sha256 = "1kg1lbgwc222x4rvgwbmfglhi74wspxvysaw1w96016pf29h3nf4";};
   }
