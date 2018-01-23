682857797f1d18cb2e9beb955a0029c34ca86e8d
Jean-Philippe Braun <eon@patapon.info>
2018-01-23 10:56:14 +0100

    Make gremlinChecksScript private

--- tools/gremlin-checks/default.nix
+++ tools/gremlin-checks/default.nix
@@ -1,15 +1,14 @@
 { contrailPkgs, pkgs, fetchurl, stdenv }:
 
-rec {
-
+let
   gremlinChecksScript = fetchurl {
     url = "https://raw.githubusercontent.com/eonpatapon/contrail-gremlin/master/gremlin-checks/checks.groovy";
     sha256 = "1p74kiakbgbsma6a45k736j5319k499zk23s9qg43mqdpx3hz8l3";
   };
-
-  gremlinChecks= pkgs.writeScriptBin "gremlin-checks" ''
-#!${pkgs.bash}/bin/bash
+in
+{
+  gremlinChecks = pkgs.writeScriptBin "gremlin-checks" ''
+#!${stdenv.shell}
 ${contrailPkgs.tools.contrailGremlin}/bin/gremlin-dump /tmp/dump.gson && ${contrailPkgs.tools.gremlinConsole}/bin/gremlin -i ${gremlinChecksScript} /tmp/dump.gson
-  '';
-
+'';
 }
