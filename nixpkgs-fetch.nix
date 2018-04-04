8aa3262f487742aff735755fe98e0f235b1aa042
Antoine Eiche <lewo@abesis.fr>
2018-04-04 12:10:07 +0200

    Update nixpkgs

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -6,6 +6,6 @@ in {
     owner = "NixOS";
     repo = "nixpkgs";
     # Belong to the branch release-18.03-cloudwatt
-    rev = "4166986dfad56fd559fe513821e029ce967e4f67";
-    sha256 = "0634zy4g5flhhjkf34lvrccrrq258dhy421n0jmy1z1gdf3r42b2";};
+    rev = "590320e107e1a047061d0a7d801830f61be38082";
+    sha256 = "0pr404mrjcwhxckkhp4qscxh73nsb3yxwy41hy6cdsvvj53zc3ji";};
   }
