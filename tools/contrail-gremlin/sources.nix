02db7e21dd42e28ea62306d86e2d402130d16c23
Jean-Philippe Braun <eon@patapon.info>
2018-04-17 11:39:08 +0200

    tools.contrailGremlin: update sources

--- tools/contrail-gremlin/sources.nix
+++ tools/contrail-gremlin/sources.nix
@@ -3,6 +3,6 @@ fetchFromGitHub:
 fetchFromGitHub {
   owner = "eonpatapon";
   repo = "contrail-gremlin";
-  rev = "8efea88ca9999eaa59a6290bab311da69011cde1";
-  sha256 = "1m38l5ql1i3fzpfmbz4y18f04pqb088bs0bj02fl1hbrlcykaygm";
+  rev = "80a074ce9b8198623281be97b4ebd8d954691a59";
+  sha256 = "0cq4x26x4fzfvhr7dsvfrv3a2fk1ya94adavi5y802fjricjjhqv";
 }
