06c6428cf350a2c31bdd3da515f5a83626d42318
Antoine Eiche <lewo@abesis.fr>
2018-10-29 15:58:05 +0100

    Update nixpkgs-fetch to 18.09

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -5,7 +5,7 @@ in {
   pkgs = bootstrap_pkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    # Belong to the branch release-18.03
-    rev = "6796f5db1c2a25aeade10d613ddb403e4eb7a928";
-    sha256 = "0fmvh1wcr1rgsr6cv8bpw5rmm9ypg94q7h9s4xqzaq567d3nhhhz";};
+    # Belong to the branch release-18.09
+    rev = "f12ea6195e4819586ee174d4ef9113b2c1007045";
+    sha256 = "07fj3bdq38fcab7acfv0ynxc849g98fd0a671apmjzfbqjg7bm68";};
   }
