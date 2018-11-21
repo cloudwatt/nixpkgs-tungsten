ebdd3433b687c12d7e626eee6ab2072794b240a7
Jean-Philippe Braun <eon@patapon.info>
2018-11-21 09:05:06 +0100

    modules.discovery: add autoStart option

--- modules/contrail-discovery.nix
+++ modules/contrail-discovery.nix
@@ -16,6 +16,10 @@ in {
         type = types.path;
         description = "The contrail discovery file path";
       };
+      autoStart = mkOption {
+        type = types.bool;
+        default = true;
+      };
       waitFor = mkOption {
         type = types.bool;
         default = true;
@@ -27,22 +31,24 @@ in {
   };
 
   config = mkIf cfg.enable {
-    systemd.services.contrail-discovery = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" "cassandra.service" "rabbitmq.service" "zookeeper.service"
-                # Keyspaces are created by the contrail-api...
-                "contrail-api.service" ];
-      requires = [ "contrail-api.service" ];
-      preStart = "mkdir -p /var/log/contrail/";
-      script = "${contrailPkgs.discovery}/bin/contrail-discovery --conf_file ${cfg.configFile}";
-      path = [ pkgs.netcat ];
-      postStart = optionalString cfg.waitFor ''
-        sleep 2
-        while ! nc -vz localhost 5998; do
+    systemd.services.contrail-discovery = mkMerge [
+      {
+        after = [ "network.target" "cassandra.service" "rabbitmq.service" "zookeeper.service"
+                  # Keyspaces are created by the contrail-api...
+                  "contrail-api.service" ];
+        requires = [ "contrail-api.service" ];
+        preStart = "mkdir -p /var/log/contrail/";
+        script = "${contrailPkgs.discovery}/bin/contrail-discovery --conf_file ${cfg.configFile}";
+        path = [ pkgs.netcat ];
+        postStart = optionalString cfg.waitFor ''
           sleep 2
-        done
-        sleep 2
-      '';
-    };
+          while ! nc -vz localhost 5998; do
+            sleep 2
+          done
+          sleep 2
+        '';
+      }
+      (mkIf cfg.autoStart { wantedBy = [ "multi-user.target" ]; })
+    ];
   };
 }
