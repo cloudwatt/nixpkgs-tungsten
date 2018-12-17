345a24c8b83186ebfb4aa1f3b8ef9c7f022899b1
Jean-Philippe Braun <eon@patapon.info>
2018-12-17 11:05:03 +0100

    ci: add declarative jobs

---
+++ ci/jobsets-declarative.nix
@@ -0,0 +1,148 @@
+{ nixpkgs, declInput, pulls }:
+let
+  pkgs = import nixpkgs {};
+  prs = builtins.fromJSON (builtins.readFile pulls);
+  prJobsets =  pkgs.lib.mapAttrs (num: info:
+    { enabled = 1;
+      hidden = false;
+      description = "PR ${num}: ${info.title}";
+      nixexprinput = "contrail";
+      nixexprpath = "jobset.nix";
+      checkinterval = 60;
+      schedulingshares = 20;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      inputs = {
+        contrail = {
+          type = "git";
+          value = "git://github.com/${info.base.repo.owner.login}/${info.base.repo.name}.git pull/${num}/head";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    }
+  ) prs;
+  desc = prJobsets // {
+    trunk = {
+      description = "Build master of nixpkgs-contrail";
+      checkinterval = 60;
+      enabled = 1;
+      nixexprinput = "contrail";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 3;
+      hidden = false;
+      inputs = {
+        contrail = {
+          value = "https://github.com/nlewo/nixpkgs-contrail master";
+          type = "git";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
+    testing = {
+      description = "Build testing branch of nixpkgs-contrail";
+      checkinterval = 60;
+      enabled = 1;
+      nixexprinput = "contrail";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        contrail = {
+          value = "https://github.com/nlewo/nixpkgs-contrail testing";
+          type = "git";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
+    staging = {
+      description = "Build master of nixpkgs-contrail and follow nixpkgs stable";
+      checkinterval = 86400;
+      enabled = 1;
+      nixexprinput = "contrail";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+        contrail = {
+          value = "https://github.com/nlewo/nixpkgs-contrail master";
+          type = "git";
+          emailresponsible = false;
+        };
+        nixpkgs = {
+          value = "https://github.com/NixOS/nixpkgs-channels nixos-18.09";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
+    unstable = {
+      description = "Build master of nixpkgs-contrail and follow nixpkgs unstable";
+      checkinterval = 86400;
+      enabled = 1;
+      nixexprinput = "contrail";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+        contrail = {
+          value = "https://github.com/nlewo/nixpkgs-contrail master";
+          type = "git";
+          emailresponsible = false;
+        };
+        nixpkgs = {
+          value = "https://github.com/NixOS/nixpkgs-channels nixos-unstable";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
+  };
+
+in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat >$out <<EOF
+    ${builtins.toJSON desc}
+    EOF
+  '';
+}
