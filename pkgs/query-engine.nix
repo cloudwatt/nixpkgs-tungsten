c823335327c16604c1bd8a5de03987a3c8018f7b
Jean-Philippe Braun <eon@patapon.info>
2019-01-21 10:07:55 +0100

    queryEngine: add build inputs

--- pkgs/query-engine.nix
+++ pkgs/query-engine.nix
@@ -1,19 +1,24 @@
 { stdenv
+, pkgs
 , deps
 , contrailVersion
-, contrailBuildInputs
 , contrailWorkspace
 }:
 
 stdenv.mkDerivation rec {
   name = "contrail-query-engine-${version}";
   version = contrailVersion;
-  buildInputs = contrailBuildInputs;
   src = contrailWorkspace;
+  buildInputs = with pkgs; [
+    scons libxml2 libtool flex_2_5_35 bison curl
+    vim # to get xxd binary required by sandesh
+    deps.boost deps.thrift deps.log4cplus deps.tbb
+    deps.cassandraCppDriver
+  ];
   USER = "contrail";
   NIX_CFLAGS_COMPILE = "-isystem ${deps.thrift}/include/thrift";
   buildPhase = ''
-    scons -j1 --optimization=production contrail-query-engine
+    scons -j2 --optimization=production contrail-query-engine
   '';
   installPhase = ''
     mkdir -p $out/{bin,etc/contrail}
