8465c54ba3c9951aef9b975e79bf9a04b2d37836
Jean-Philippe Braun <eon@patapon.info>
2019-01-21 12:55:27 +0100

    control: add build inputs

--- pkgs/control.nix
+++ pkgs/control.nix
@@ -2,26 +2,30 @@
 , stdenv
 , deps
 , contrailVersion
-, contrailBuildInputs
 , contrailWorkspace
-, isContrailMaster }:
+, isContrailMaster
+}:
+
+with pkgs.lib;
 
 stdenv.mkDerivation rec {
   name = "contrail-control-${version}";
   version = contrailVersion;
   src = contrailWorkspace;
+  buildInputs = with pkgs; [
+    scons libxml2 libtool flex_2_5_35 bison curl
+    vim # to get xxd binary required by sandesh
+    deps.thrift deps.boost deps.log4cplus deps.tbb
+    pythonPackages.lxml
+  ] ++ (optional isContrailMaster [
+    cmake rabbitmq-c gperftools
+  ]);
   USER = "contrail";
   # Only required on master
   dontUseCmakeConfigure = true;
-
   NIX_CFLAGS_COMPILE = "-isystem ${deps.thrift}/include/thrift";
-
-  buildInputs = with pkgs;
-    contrailBuildInputs ++
-    (pkgs.lib.optional isContrailMaster [ cmake rabbitmq-c gperftools ]);
-
   buildPhase = ''
-    scons -j1 --optimization=production contrail-control
+    scons -j2 --optimization=production contrail-control
   '';
   installPhase = ''
     mkdir -p $out/{bin,etc/contrail}
