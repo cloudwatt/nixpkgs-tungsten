7bd2eebdffe7bf428b88f5077ce64a9ce512b1de
Jean-Philippe Braun <eon@patapon.info>
2019-01-22 10:16:09 +0100

    pythonBuild: fix for nix-shell use
    Extend PYTHONPATH and propagatedBuildInputs so that api-server,
    svc-monitor, schema-transformer etc... can be run in nix-shell.

--- pkgs/python-build.nix
+++ pkgs/python-build.nix
@@ -10,32 +10,29 @@
 with pkgs.lib;
 
 stdenv.mkDerivation rec {
-  name = "contrail-python";
+  name = "contrail-python-${version}";
   version = contrailVersion;
   src = contrailWorkspace;
   USER = "contrail";
-  # Only required on master
-  dontUseCmakeConfigure = true;
-
   buildInputs =
     (with pkgs; [
       scons libxml2 flex_2_5_35 bison curl
       vim # to get xxd binary required by sandesh
       deps.boost deps.tbb deps.log4cplus
-      breakpointHook
     ]) ++
     (with pythonPackages; [
       lxml
       bitarray pbr funcsigs mock bottle requests # for tests
     ]);
 
-  propagatedBuildInputs = with pythonPackages; [
-    psutil geventhttpclient
-  ];
-
   prePatch = ''
+    # Avoid running system sphinx when build is not sandboxed
+    substituteInPlace controller/src/config/api-server/doc/SConscript \
+      --replace "/usr/bin/sphinx-apidoc" "/doesnt-exists"
+
     # Don't know if this test is supposed to pass
-    substituteInPlace controller/src/config/common/tests/test_analytics_client.py --replace "test_analytics_request_with_data" "nop"
+    substituteInPlace controller/src/config/common/tests/test_analytics_client.py \
+      --replace "test_analytics_request_with_data" "nop"
 
     # It seems these tests require contrail-test repository to be executed
     # See https://github.com/Juniper/contrail-test/wiki/Running-Tests
@@ -45,19 +42,28 @@ stdenv.mkDerivation rec {
 
     # Tests are disabled because they requires to compile vizd (collector)
     sed -i '/OpEnv.AlwaysBuild(test_cmd)/d' controller/src/opserver/SConscript
-  '' + (optionalString isContrailMaster ''
+  '' + optionalString isContrailMaster ''
     substituteInPlace controller/src/config/common/setup.py --replace "test_suite='tests.test_suite'," ""
-  '');
-
-  buildPhase = ''
-    export PYTHONPATH=$PYTHONPATH:controller/src/config/common/:build/production/config/api-server/vnc_cfg_api_server/gen/
-    scons -j1 --optimization=production controller/src/config
-
-    scons -j1 --optimization=production contrail-analytics-api
-    ${optionalString isContrail32 "scons -j1 --optimization=production contrail-discovery"}
   '';
 
-  installPhase = ''
-    ${optionalString isContrailMaster "rm build/third_party/thrift/lib/cpp/.libs/concurrency_test"}
-    mkdir $out; cp -r build/* $out'';
+  # Deps to run api-server, schema-transformer, etc... in nix-shell
+  PYTHONPATH = "build/production/config/common:build/production/sandesh/common:build/production/tools/sandesh/library/python:build/production/api-lib:build/production/discovery/client:build/production/config/schema-transformer:build/production/config/vnc_openstack:build/production/config/api-server/vnc_cfg_api_server/gen:controller/src/config/common";
+  propagatedBuildInputs = with pythonPackages; [
+    netaddr psutil bitarray pycassa lxml geventhttpclient
+    kazoo kombu pyopenssl stevedore netifaces keystonemiddleware
+    jsonpickle pyyaml xmltodict gevent bottle requests neutron_constants
+  ];
+
+  buildPhase = ''
+    scons -j1 --optimization=production controller/src/config
+    scons -j1 --optimization=production contrail-analytics-api
+  '' + optionalString isContrail32 ''
+    scons -j1 --optimization=production contrail-discovery
+  '';
+
+  installPhase = optionalString isContrailMaster ''
+    rm build/third_party/thrift/lib/cpp/.libs/concurrency_test
+  '' + ''
+    mkdir $out; cp -r build/* $out
+  '';
 }
